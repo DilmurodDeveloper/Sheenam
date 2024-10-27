93aa2f100e45e027c26b674c03bc4bfb17c906db
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2024-10-27 22:02:02 +0500

    INFRA: Version Changes

--- Sheenam.Api.Infrastracture.Build/Program.cs
+++ Sheenam.Api.Infrastracture.Build/Program.cs
@@ -48,7 +48,7 @@ var githubPipeline = new GithubPipeline
 
                         TargetDotNetVersion = new TargetDotNetVersion
                         {
-                            DotNetVersion = "6.0.427"
+                            DotNetVersion = "6.0.35"
                         }
                     },
 
