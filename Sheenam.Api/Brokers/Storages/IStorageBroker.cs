e580204d4abf82c2ab116700057fe72c8463898b
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2024-10-30 13:53:44 +0500

    BROKERS: Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.cs
@@ -0,0 +1,9 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace   
+//=================================================
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker {}
+}
