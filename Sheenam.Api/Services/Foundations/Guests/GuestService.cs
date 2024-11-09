cafa4999f3c3604b9848a5e25469c3c8ff9cec9e
Dilmurod Madirimov <madirimovdilmurod2@gmail.com>
2024-11-09 22:29:17 +0500

    CODE RUB: Code changes

--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -7,6 +7,7 @@ using System;
 using System.Threading.Tasks;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Guests;
+using Sheenam.Api.Models.Foundations.Guests.Exceptions;
 
 namespace Sheenam.Api.Services.Foundations.Guests
 {
@@ -17,7 +18,14 @@ namespace Sheenam.Api.Services.Foundations.Guests
         public GuestService(IStorageBroker storageBroker) => 
             this.storageBroker = storageBroker;
 
-        public async ValueTask<Guest> AddGuestAsync(Guest guest) =>
-            await this.storageBroker.InsertGuestAsync(guest);
+        public async ValueTask<Guest> AddGuestAsync(Guest guest)
+        {
+            if (guest == null)
+            {
+                throw new GuestValidationException(new NullGuestException());
+            }
+
+            return await this.storageBroker.InsertGuestAsync(guest);
+        }
     }
 }
