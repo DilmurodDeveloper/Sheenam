598352a03fce095ea519f7ae2239b23c2768a93d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-16 21:26:28 +0500

    ShouldThrowCriticalDependencyExceptionOnAddIfSqlErrorOccursAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.cs
@@ -4,6 +4,8 @@
 // = = = = = = = = = = = = = = = = = = = = = = = = = 
 
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using Microsoft.Data.SqlClient;
 using Moq;
 using Sheenam.Api.Brokers.DateTimes;
 using Sheenam.Api.Brokers.Loggings;
@@ -38,6 +40,9 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
 
         private static DateTimeOffset GetRandomDateTimeOffset() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static SqlException GetSqlError() =>
+            (SqlException)RuntimeHelpers.GetUninitializedObject(typeof(SqlException));
 
         private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
             actualException => actualException.SameExceptionAs(expectedException);
