dad9961031d2be75ed3b14ac25c3003111a68cec
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-16 18:44:16 +0500

    BROKERS: Insert HomeRequest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.HomeRequest.cs
@@ -0,0 +1,14 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using Sheenam.Api.Models.Foundations.HomeRequests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<HomeRequest> InsertHomeRequestAsync(HomeRequest homeRequest);
+    }
+}
--- Sheenam.Api/Brokers/Storages/StorageBroker.HomeRequest.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.HomeRequest.cs
@@ -4,6 +4,7 @@
 // = = = = = = = = = = = = = = = = = = = = = = = = = 
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Sheenam.Api.Models.Foundations.HomeRequests;
 
 namespace Sheenam.Api.Brokers.Storages
@@ -11,5 +12,17 @@ namespace Sheenam.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<HomeRequest> HomeRequests { get; set; }
+
+        public async ValueTask<HomeRequest> InsertHomeRequestAsync(HomeRequest homeRequest)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<HomeRequest> homeRequestEntityEntry =
+                await broker.HomeRequests.AddAsync(homeRequest);
+
+            await broker.SaveChangesAsync();
+
+            return homeRequestEntityEntry.Entity;
+        }
     }
 }
