dc958f94f84d6b4fe8c15db90a035cc12b5dd31d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-16 19:10:34 +0500

    ShouldAddHomeRequestAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.cs
@@ -0,0 +1,50 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using Moq;
+using Sheenam.Api.Brokers.DateTimes;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+using Sheenam.Api.Services.Foundations.HomeRequests;
+using Tynamix.ObjectFiller;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        private readonly IHomeRequestService homeRequestService;
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        
+        public HomeRequestServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+            this.homeRequestService = new HomeRequestService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static HomeRequest CreateRandomHomeRequest() =>
+            CreateHomeRequestFiller(date: GetRandomDateTimeOffset()).Create();
+
+        private static DateTimeOffset GetRandomDateTimeOffset() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Filler<HomeRequest> CreateHomeRequestFiller(DateTimeOffset date)
+        {
+            var filler = new Filler<HomeRequest>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(date);
+            
+            return filler;
+        }
+    }
+}
