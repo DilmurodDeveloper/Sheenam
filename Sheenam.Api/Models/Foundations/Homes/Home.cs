ef32d3d838c093c0a39842dbf62a4033f6656b48
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-16 18:23:34 +0500

    DATA: Model & Migrate HomeRequests

---
+++ Sheenam.Api/Models/Foundations/Homes/Home.cs
@@ -0,0 +1,12 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+namespace Sheenam.Api.Models.Foundations.Homes
+{
+    public class Home
+    {
+        public Guid Id { get; set; }
+    }
+}
