e5fdc8437624db87de2b6274b1c37dc8effd6a0d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-17 19:49:46 +0500

    ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
+++ Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
@@ -82,6 +82,18 @@ namespace Sheenam.Api.Services.Foundations.HomeRequests
 
                 throw homeRequestDependencyException;
             }
+            catch (Exception exception)
+            {
+                var failedHomeRequestServiceException =
+                    new FailedHomeRequestServiceException(exception);
+
+                var homeRequestServiceException =
+                    new HomeRequestServiceException(failedHomeRequestServiceException);
+
+                this.loggingBroker.LogError(homeRequestServiceException);
+
+                throw homeRequestServiceException;
+            }
         }
     }
 }
