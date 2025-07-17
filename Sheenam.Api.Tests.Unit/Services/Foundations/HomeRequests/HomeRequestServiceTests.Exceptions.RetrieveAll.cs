65539fc2f2d9e2207530b090aa0eb868fb35ea1f
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-17 17:28:08 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,56 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.HomeRequests.Exceptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt()
+        {
+            // given
+            var sqlException = GetSqlError();
+
+            var failedHomeRequestStorageException =
+                new FailedHomeRequestStorageException(sqlException);
+
+            var expectedHomeRequestDependencyException =
+                new HomeRequestDependencyException(failedHomeRequestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllHomeRequests())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllHomeRequestsAction = () =>
+                this.homeRequestService.RetrieveAllHomeRequests();
+
+            HomeRequestDependencyException actualHomeRequestDependencyException =
+                Assert.Throws<HomeRequestDependencyException>(
+                    retrieveAllHomeRequestsAction);
+
+            // then
+            actualHomeRequestDependencyException.Should()
+                .BeEquivalentTo(expectedHomeRequestDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllHomeRequests(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedHomeRequestDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
