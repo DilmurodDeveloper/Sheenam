df599983bace5e3ba064c772cd40d4dc08a31ff9
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-17 23:55:56 +0500

    ShouldThrowNotFoundExceptionOnRemoveHomeRequestByIdIsNotFoundAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
+++ Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
@@ -73,12 +73,23 @@ namespace Sheenam.Api.Services.Foundations.HomeRequests
                 HomeRequest maybeHomeRequest =
                 await this.storageBroker.SelectHomeRequestByIdAsync(homeRequestId);
 
+                ValidateStorageHomeRequestIsNotNull(maybeHomeRequest, homeRequestId);
+
                 return await this.storageBroker.DeleteHomeRequestAsync(maybeHomeRequest);
             }
             catch (InvalidHomeRequestException invalidHomeRequestException)
             {
                 var homeRequestValidationException =
                     new HomeRequestValidationException(invalidHomeRequestException);
+
+                this.loggingBroker.LogError(homeRequestValidationException);
+
+                throw homeRequestValidationException;
+            }
+            catch (NotFoundHomeRequestException notFoundHomeRequestException)
+            {
+                var homeRequestValidationException =
+                    new HomeRequestValidationException(notFoundHomeRequestException);
 
                 this.loggingBroker.LogError(homeRequestValidationException);
 
