ca9eacddcd87db9d64c2a7c3a3f0ce0405b7d0bc
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-17 17:30:28 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllWhenSqlExceptionOccursAndLogIt -> PASS

--- Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
+++ Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
@@ -3,10 +3,12 @@
 // Free To Use To Find Comfort and Peace    
 // = = = = = = = = = = = = = = = = = = = = = = = = = 
 
+using Microsoft.Data.SqlClient;
 using Sheenam.Api.Brokers.DateTimes;
 using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.HomeRequests;
+using Sheenam.Api.Models.Foundations.HomeRequests.Exceptions;
 
 namespace Sheenam.Api.Services.Foundations.HomeRequests
 {
@@ -34,7 +36,24 @@ namespace Sheenam.Api.Services.Foundations.HomeRequests
             return await this.storageBroker.InsertHomeRequestAsync(homeRequest);
         });
 
-        public IQueryable<HomeRequest> RetrieveAllHomeRequests() =>
-            this.storageBroker.SelectAllHomeRequests();
+        public IQueryable<HomeRequest> RetrieveAllHomeRequests()
+        {
+            try
+            {
+                return this.storageBroker.SelectAllHomeRequests();
+            }
+            catch (SqlException sqlException)
+            {
+                var failedHomeRequestStorageException =
+                    new FailedHomeRequestStorageException(sqlException);
+
+                var homeRequestDependencyException =
+                    new HomeRequestDependencyException(failedHomeRequestStorageException);
+
+                this.loggingBroker.LogCritical(homeRequestDependencyException);
+
+                throw homeRequestDependencyException;
+            }
+        }
     }
 }
