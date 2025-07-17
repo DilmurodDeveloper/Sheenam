689a32814d073c28540b91197b65830bb5b20c05
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-17 21:34:39 +0500

    ShouldThrowValidationExceptionOnModifyIfHomeRequestMessageIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Validations.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Validations.Modify.cs
@@ -47,5 +47,81 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public async Task ShouldThrowValidationExceptionOnModifyIfHomeRequestMessageIsInvalidAndLogItAsync(
+            string invalidMessage)
+        {
+            // given
+            HomeRequest invalidHomeRequest = new()
+            {
+                Message = invalidMessage
+            };
+
+            var invalidHomeRequestException = new InvalidHomeRequestException();
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.Id),
+                values: "Id is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.GuestId),
+                values: "Guest Id is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.HomeId),
+                values: "Home Id is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.Message),
+                values: "Text is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.StartDate),
+                values: "Date is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.EndDate),
+                values: "Date is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.CreatedDate),
+                values: "Date is required");
+
+            invalidHomeRequestException.AddData(
+                key: nameof(HomeRequest.UpdatedDate),
+                values: "Date is required");
+
+            var expectedHomeRequestValidationException =
+                new HomeRequestValidationException(invalidHomeRequestException);
+
+            // when
+            ValueTask<HomeRequest> modifyHomeRequestTask =
+                this.homeRequestService.ModifyHomeRequestAsync(invalidHomeRequest);
+
+            HomeRequestValidationException actualHomeRequestValidationException =
+                await Assert.ThrowsAsync<HomeRequestValidationException>(() =>
+                    modifyHomeRequestTask.AsTask());
+
+            // then
+            actualHomeRequestValidationException.Should()
+                .BeEquivalentTo(expectedHomeRequestValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeRequestValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateHomeRequestAsync(It.IsAny<HomeRequest>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
