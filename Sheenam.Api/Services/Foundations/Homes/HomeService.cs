ec8bc08bd0636c1b66b9d86c6beffad7b0428daf
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 19:51:56 +0500

    CODE RUB: Implement TryCatch

--- Sheenam.Api/Services/Foundations/Homes/HomeService.cs
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -3,13 +3,10 @@
 // Free To Use To Find Comfort and Peace    
 // = = = = = = = = = = = = = = = = = = = = = = = = = 
 
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 using Sheenam.Api.Brokers.DateTimes;
 using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
 using Sheenam.Api.Models.Foundations.Homes;
-using Sheenam.Api.Models.Foundations.Homes.Exceptions;
 
 namespace Sheenam.Api.Services.Foundations.Homes
 {
@@ -50,94 +47,17 @@ namespace Sheenam.Api.Services.Foundations.Homes
             return maybeHome;
         });
 
-        public async ValueTask<Home> ModifyHomeAsync(Home home)
+        public ValueTask<Home> ModifyHomeAsync(Home home) =>
+        TryCatch(async () =>
         {
-            try
-            {
-                ValidateHomeOnModify(home);
+            ValidateHomeOnModify(home);
 
-                Home maybeHome =
-                await this.storageBroker.SelectHomeByIdAsync(home.Id);
+            Home maybeHome =
+            await this.storageBroker.SelectHomeByIdAsync(home.Id);
 
-                ValidateAgainstStorageHomeOnModify(home, maybeHome);
+            ValidateAgainstStorageHomeOnModify(home, maybeHome);
 
-                return await this.storageBroker.UpdateHomeAsync(home);
-            }
-            catch (NullHomeException nullHomeException)
-            {
-                var homeValidationException =
-                    new HomeValidationException(nullHomeException);
-
-                this.loggingBroker.LogError(homeValidationException);
-
-                throw homeValidationException;
-            }
-            catch (InvalidHomeException invalidHomeException)
-            {
-                var homeValidationException =
-                    new HomeValidationException(invalidHomeException);
-
-                this.loggingBroker.LogError(homeValidationException);
-
-                throw homeValidationException;
-            }
-            catch (NotFoundHomeException notFoundHomeException)
-            {
-                var homeValidationException =
-                    new HomeValidationException(notFoundHomeException);
-
-                this.loggingBroker.LogError(homeValidationException);
-
-                throw homeValidationException;
-            }
-            catch (SqlException sqlException)
-            {
-                var failedHomeStorageException =
-                    new FailedHomeStorageException(sqlException);
-
-                var homeDependencyException =
-                    new HomeDependencyException(failedHomeStorageException);
-
-                this.loggingBroker.LogCritical(homeDependencyException);
-
-                throw homeDependencyException;
-            }
-            catch (DbUpdateConcurrencyException dbUpdateConcurrencyException)
-            {
-                var lockedHomeException =
-                new LockedHomeException(dbUpdateConcurrencyException);
-
-                var homeDependencyValidationException =
-                    new HomeDependencyValidationException(lockedHomeException);
-
-                this.loggingBroker.LogError(homeDependencyValidationException);
-
-                throw homeDependencyValidationException;
-            }
-            catch (DbUpdateException dbUpdateException)
-            {
-                var failedHomeStorageException =
-                    new FailedHomeStorageException(dbUpdateException);
-
-                var homeDependencyException =
-                    new HomeDependencyException(failedHomeStorageException);
-
-                this.loggingBroker.LogError(homeDependencyException);
-
-                throw homeDependencyException;
-            }
-            catch (Exception exception)
-            {
-                var failedHomeServiceException =
-                new FailedHomeServiceException(exception);
-
-                var homeServiceException =
-                    new HomeServiceException(failedHomeServiceException);
-
-                this.loggingBroker.LogError(homeServiceException);
-
-                throw homeServiceException;
-            }
-        }
+            return await this.storageBroker.UpdateHomeAsync(home);
+        });
     }
 }
