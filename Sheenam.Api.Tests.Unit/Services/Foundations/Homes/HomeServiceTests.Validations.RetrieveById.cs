346a4c4843f6318241d00862a236ae8857aeb88a
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 17:08:36 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfHomeNotFoundAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Validations.RetrieveById.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Validations.RetrieveById.cs
@@ -51,5 +51,48 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfHomeNotFoundAndLogItAsync()
+        {
+            // given
+            Guid someHomeId = Guid.NewGuid();
+            Home noHome = null;
+
+            var notFoundHomeException =
+                new NotFoundHomeException(someHomeId);
+
+            var expectedHomeValidationException =
+                new HomeValidationException(notFoundHomeException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeByIdAsync(It.IsAny<Guid>()))
+                    .ReturnsAsync(noHome);
+
+            // when
+            ValueTask<Home> retrieveHomeByIdTask =
+                this.homeService.RetrieveHomeByIdAsync(someHomeId);
+
+            HomeValidationException actualHomeValidationException =
+                await Assert.ThrowsAsync<HomeValidationException>(() =>
+                    retrieveHomeByIdTask.AsTask());
+
+            // then
+            actualHomeValidationException.Should()
+                .BeEquivalentTo(expectedHomeValidationException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(someHomeId),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
