8619851432188d5cdcbb6557c256e0082eefebba
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 17:23:14 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Exceptions.RetrieveById.cs
@@ -0,0 +1,59 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.Homes;
+using Sheenam.Api.Models.Foundations.Homes.Exceptions;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someHomeId = Guid.NewGuid();
+            SqlException sqlException = GetSqlError();
+
+            var failedHomeStorageException =
+                new FailedHomeStorageException(sqlException);
+
+            var expectedHomeDependencyException =
+                new HomeDependencyException(failedHomeStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<Home> retrieveHomeById =
+                this.homeService.RetrieveHomeByIdAsync(someHomeId);
+
+            HomeDependencyException actualHomeDependencyException =
+                await Assert.ThrowsAsync<HomeDependencyException>(() =>
+                    retrieveHomeById.AsTask());
+
+            // then
+            actualHomeDependencyException.Should()
+                .BeEquivalentTo(expectedHomeDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(someHomeId),
+                    Times.Once());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedHomeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
