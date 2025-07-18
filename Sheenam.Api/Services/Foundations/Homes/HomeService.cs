20653e25723e9fef9413e7163d2fcabb9162f352
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 12:51:37 +0500

    ShouldAddHomeAsync -> FAIL

---
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -0,0 +1,32 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using Sheenam.Api.Brokers.DateTimes;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Homes;
+
+namespace Sheenam.Api.Services.Foundations.Homes
+{
+    public class HomeService : IHomeService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IDateTimeBroker dateTimeBroker;
+
+        public HomeService(
+            IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker,
+            IDateTimeBroker dateTimeBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+            this.dateTimeBroker = dateTimeBroker;
+        }
+
+        public ValueTask<Home> AddHomeAsync(Home home) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ Sheenam.Api/Services/Foundations/Homes/IHomeService.cs
@@ -0,0 +1,14 @@
+﻿// = = = = = = = = = = = = = = = = = = = = = = = = = 
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace    
+// = = = = = = = = = = = = = = = = = = = = = = = = = 
+
+using Sheenam.Api.Models.Foundations.Homes;
+
+namespace Sheenam.Api.Services.Foundations.Homes
+{
+    public interface IHomeService
+    {
+        ValueTask<Home> AddHomeAsync(Home home);
+    }
+}
