b2bc34daf4a97f382ab7fbf25c2ac8b99629fb69
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 22:05:37 +0500

    ShouldThrowServiceExceptionOnRemoveIfExceptionOccursAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Homes/HomeService.cs
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -118,6 +118,18 @@ namespace Sheenam.Api.Services.Foundations.Homes
 
                 throw homeDependencyException;
             }
+            catch (Exception exception)
+            {
+                var failedHomeServiceException =
+                    new FailedHomeServiceException(exception);
+
+                var homeServiceException =
+                    new HomeServiceException(failedHomeServiceException);
+
+                this.loggingBroker.LogError(homeServiceException);
+
+                throw homeServiceException;
+            }
         }
     }
 }
