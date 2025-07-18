71156a3ece4ee43d31e391b26ad80ee75201aefc
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 19:09:37 +0500

    ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Exceptions.Modify.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Exceptions.Modify.cs
@@ -5,6 +5,7 @@
 
 using FluentAssertions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 using Sheenam.Api.Models.Foundations.Homes;
 using Sheenam.Api.Models.Foundations.Homes.Exceptions;
@@ -45,6 +46,54 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
 
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedHomeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(homeId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.UpdateHomeAsync(someHome),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnModifyIfDatabaseUpdateExceptionOccursAndLogItAsync()
+        {
+            // given
+            Home randomHome = CreateRandomHome();
+            Home someHome = randomHome;
+            Guid homeId = someHome.Id;
+            var dbUpdateException = new DbUpdateException();
+
+            var failedHomeStorageException =
+                new FailedHomeStorageException(dbUpdateException);
+
+            var expectedHomeDependencyException =
+                new HomeDependencyException(failedHomeStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeByIdAsync(homeId)).Throws(dbUpdateException);
+
+            // when
+            ValueTask<Home> modifyHomeTask =
+                this.homeService.ModifyHomeAsync(someHome);
+
+            HomeDependencyException actualHomeDependencyException =
+                await Assert.ThrowsAsync<HomeDependencyException>(() =>
+                    modifyHomeTask.AsTask());
+
+            // then
+            actualHomeDependencyException.Should()
+                .BeEquivalentTo(expectedHomeDependencyException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
                     expectedHomeDependencyException))),
                         Times.Once);
 
