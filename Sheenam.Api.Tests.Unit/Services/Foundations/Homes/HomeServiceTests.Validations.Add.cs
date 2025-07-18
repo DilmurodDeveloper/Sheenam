32e43e17af3f1dbb7adf0d5988583443ae99bb6d
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 14:45:19 +0500

    ShouldThrowValidationExceptionOnAddIfTypeIsInvalidAndLogItAsync -> FAIL

--- Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Validations.Add.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Validations.Add.cs
@@ -114,5 +114,43 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfTypeIsInvalidAndLogItAsync()
+        {
+            // given
+            Home randomHome = CreateRandomHome();
+            Home invalidHome = randomHome;
+            invalidHome.Type = GetInvalidEnum<HouseType>();
+            var invalidHomeException = new InvalidHomeException();
+
+            invalidHomeException.AddData(
+                key: nameof(Home.Type),
+                values: "Type is required");
+
+            var expectedHomeValidationException =
+                new HomeValidationException(invalidHomeException);
+
+            // when
+            ValueTask<Home> addHomeTask =
+                this.homeService.AddHomeAsync(invalidHome);
+
+            // then
+            await Assert.ThrowsAsync<HomeValidationException>(() =>
+                addHomeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertHomeAsync(It.IsAny<Home>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
--- Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.cs
@@ -42,6 +42,18 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
         private static int GetRandomNumber() =>
             new IntRange(min: 2, max: 9).GetValue();
 
+        private static T GetInvalidEnum<T>()
+        {
+            int randomNumber = GetRandomNumber();
+
+            while (Enum.IsDefined(typeof(T), randomNumber) is true)
+            {
+                randomNumber = GetRandomNumber();
+            }
+
+            return (T)(object)randomNumber;
+        }
+
         private Expression<Func<Xeption, bool>> SameExceptionAs(Xeption expectedException) =>
             actualException => actualException.SameExceptionAs(expectedException);
 
