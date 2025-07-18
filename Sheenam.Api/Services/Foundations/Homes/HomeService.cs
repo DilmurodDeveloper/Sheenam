318c90aaf05e42bfe54c6ffbad14a571cf3ab3d1
DilmurodDeveloper <madirimovdilmurod2@gmail.com>
2025-07-18 17:26:26 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveByIdIfSqlErrorOccursAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/Homes/HomeService.cs
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort and Peace    
 // = = = = = = = = = = = = = = = = = = = = = = = = = 
 
+using Microsoft.Data.SqlClient;
 using Sheenam.Api.Brokers.DateTimes;
 using Sheenam.Api.Brokers.Loggings;
 using Sheenam.Api.Brokers.Storages;
@@ -67,6 +68,18 @@ namespace Sheenam.Api.Services.Foundations.Homes
                 this.loggingBroker.LogError(homeValidationException);
 
                 throw homeValidationException;
+            }
+            catch (SqlException sqlException)
+            {
+                var failedHomeStorageException =
+                    new FailedHomeStorageException(sqlException);
+
+                var homeDependencyException =
+                    new HomeDependencyException(failedHomeStorageException);
+
+                this.loggingBroker.LogCritical(homeDependencyException);
+
+                throw homeDependencyException;
             }
         }
     }
